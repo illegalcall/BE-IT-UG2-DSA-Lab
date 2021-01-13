baea042c0ff11f8a77c9ee168ad9e3aa20dc1481
soumitri2001 <56516406+soumitri2001@users.noreply.github.com>
2021-01-13 18:12:33 +0530

    merge sort.

---
+++ DSA Assignment Set 3/4e_MergeSort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergeSort(int arr[], int size);
+void mergeArrays(int left[], int nL, int right[], int nR, int arr[]);
+
+int size;
+
+int main()
+{
+    int arr[] = {4, 6, 1, 3, 2, 7, 8, 9, 5, 10, 2, 6};
+    size = sizeof(arr) / sizeof(arr[0]);
+    mergeSort(arr, size);
+    printf("Final sorted array:\n");
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+void mergeSort(int arr[], int size)
+{
+    if (size < 2)
+        return;
+
+    int i, mid = size / 2, nL = mid, nR = size - mid;
+    int *left = (int *)malloc(sizeof(int) * nL);
+    int *right = (int *)malloc(sizeof(int) * nR);
+    for (i = 0; i < mid; i++)
+    {
+        left[i] = arr[i];
+    }
+    for (i = mid; i < size; i++)
+    {
+        right[i - mid] = arr[i];
+    }
+    mergeSort(left, nL);
+    mergeSort(right, nR);
+    mergeArrays(left, nL, right, nR, arr);
+}
+
+void mergeArrays(int *left, int nL, int *right, int nR, int arr[])
+{
+    int i = 0, j = 0, k = 0;
+    while (i < nL && j < nR)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k++] = left[i++];
+        }
+        else
+        {
+            arr[k++] = right[j++];
+        }
+    }
+    while (i < nL)
+    {
+        arr[k++] = left[i++];
+    }
+    while (j < nR)
+    {
+        arr[k++] = right[j++];
+    }
+}
